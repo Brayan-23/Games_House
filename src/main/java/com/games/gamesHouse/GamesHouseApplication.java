e12fb42b79378f4f2275c9e6043d96a0b1c4f2c7
Brayan Santos <brayan.santosrosa@gmail.com>
2023-09-04 09:39:58 -0300

    Start Project Games House, lets go!!!

---
+++ src/main/java/com/games/gamesHouse/GamesHouseApplication.java
@@ -0,0 +1,13 @@
+package com.games.gamesHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/games/gamesHouse/GamesHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.gamesHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
